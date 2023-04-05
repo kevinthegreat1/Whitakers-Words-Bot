94907a7bb41b576a7587ef6c97bd2de0f6f651f6
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-04-05 14:15:16 -0400

    Updated token error handling

--- src/main/java/com/kevinthegreat/WhitakersWordsBot.java
+++ src/main/java/com/kevinthegreat/WhitakersWordsBot.java
@@ -26,7 +26,8 @@ public class WhitakersWordsBot extends ListenerAdapter {
 
     public static void main(String[] args) {
         if (args.length < 1 || args[0].length() != 72) {
-            System.out.println("Please provide the token as the first argument.");
+            LOGGER.error("Please provide the token as the first argument.");
+            return;
         }
         JDA jda = JDABuilder.createLight(args[0], Collections.emptyList()).addEventListeners(new WhitakersWordsBot()).setActivity(Activity.playing("Type /word")).build();
         jda.updateCommands().addCommands(Commands.slash("word", "Search up a word in Whitaker's Words").addOption(OptionType.STRING, "word", "The word to search up", true)).queue();
